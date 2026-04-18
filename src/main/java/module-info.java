18544823c888a515ad8515ca089c4a3c983b743c
caduuhidee <rangedbossman@gmail.com>
2026-04-18 18:23:40 -0400

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.group4.sprint2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.group4.sprint2 to javafx.fxml;
+    exports com.group4.sprint2;
+}
