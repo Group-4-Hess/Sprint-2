092dc8204ba0174b1f3b1967264622bfbaf26c56
caduhidee <c.hyde1117@gmail.com>
2026-04-18 18:52:38 -0400

    login screen creation

--- src/main/java/com/group4/sprint2/Sprint_2.java
+++ src/main/java/com/group4/sprint2/Sprint_2.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Sprint_2 extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Sprint_2.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Sprint_2.class.getResource("log-in screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
