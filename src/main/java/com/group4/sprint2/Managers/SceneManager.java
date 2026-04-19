c8cacbeac57cc0ec9f81da8a947916565c2d5720
caduhidee <c.hyde1117@gmail.com>
2026-04-19 00:00:16 -0400

    creating scene manager and login screen

---
+++ src/main/java/com/group4/sprint2/Managers/SceneManager.java
@@ -0,0 +1,27 @@
+package com.group4.sprint2.Managers;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneManager {
+    private static Stage stage;
+
+    public static void setStage(Stage stage) {
+        SceneManager.stage = stage;
+    }
+
+    public static void switchScene(String fxmlFile) throws IOException{
+        // Code to switch scenes using the provided FXML file
+        FXMLLoader fxmlLoader = new FXMLLoader(SceneManager.class.getResource("com/group4/sprint2/" + fxmlFile));
+        Parent root = fxmlLoader.load();
+        stage.setScene(new Scene(root));
+        stage.show();
+        
+    }
+
+
+}
